cf731c8ffdc99bd8dfde43c28d707e4586c95d1b
nicolassack5 <nicolassack2002@gmail.com>
2026-09-21 01:42:13 -0300

    Corrección de error de url

--- app/src/main/java/com/example/myapplication/network/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/network/RetrofitClient.java
@@ -14,7 +14,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.168.100.48:8081/";
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
 
     private static Retrofit retrofit;
 
